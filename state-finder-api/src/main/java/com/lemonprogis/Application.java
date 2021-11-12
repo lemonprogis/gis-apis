f8395cee057a675cbcc3cf688ebdf77548e87d31
Ed Briggler <ed.briggler@jbhunt.com>
2021-11-11 21:43:00 -0600

    got the file part straight for local and in container

--- state-finder-api/src/main/java/com/lemonprogis/Application.java
+++ state-finder-api/src/main/java/com/lemonprogis/Application.java
@@ -2,29 +2,29 @@ package com.lemonprogis;
 
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
-	public DataStore stateDataStore() throws IOException {
-		Resource resource = new ClassPathResource("us_states/cb_2018_us_state_500k.shp");
-		resource.getFile();
+	public DataStore stateDataStore(
+			@Value("${data.shapefiles.us-states}") String usStatesShapefile) throws IOException {
+		File shp = ResourceUtils.getFile(usStatesShapefile);
 		Map<String, Object> map = new HashMap<>();
-		map.put("url", resource.getFile().toURI().toURL());
+		map.put("url", shp.toURI().toURL());
 		return DataStoreFinder.getDataStore(map);
 	}
 }
