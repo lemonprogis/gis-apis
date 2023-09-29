48671bb4bf407f2f53293215c06ac3ca4056a0ff
Ed Briggler <ed.briggler@jbhunt.com>
2023-09-29 14:16:04 -0500

    upgrades and clean up

--- state-finder-api/src/test/java/com/lemonprogis/ApplicationIT.java
+++ state-finder-api/src/test/java/com/lemonprogis/ApplicationIT.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
